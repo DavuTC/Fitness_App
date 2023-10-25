1a2db0f7564ba0dcd155cfef05bc68c2a4dda93b
DavuTC <c.oglud52@gmail.com>
2023-10-25 19:56:36 +0300

    bacak class ok

--- Screens/BacakScreen.jsx
+++ Screens/BacakScreen.jsx
@@ -1,17 +1,108 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text,Button, TouchableOpacity,StyleSheet } from 'react-native';
+
 
 function BacakScreen({ navigation }) {
   return (
-    <View>
-      <Text>Menü Sayfası</Text>
+    <View style= {StyleSheet.Container}>
+
+      <View style={styles.buttonContainer}>
+      
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Jump Squat')}
+      >
+        <Text style={styles.buttonText}>Jump Squat</Text>
+      </TouchableOpacity> 
+
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Lying Curl')}
+      >
+        <Text style={styles.buttonText}>Lying Curl</Text>
+      </TouchableOpacity>
+
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Standing Calf Raise')}
+      >
+        <Text style={styles.buttonText}>Standing Calf Raise</Text>
+      </TouchableOpacity>
+
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Sumo Squat')}
+      >
+        <Text style={styles.buttonText}>Sumo Squat</Text>
+      </TouchableOpacity>
+
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Thigh Adductor')}
+      >
+        <Text style={styles.buttonText}>ThighAdductor</Text>
+      </TouchableOpacity>
+
+
+            
+      
+      
+      {/* Diğer menü öğeleri ve yönlendirmeleri */}
+      </View>
       <Button
         title="Ana Sayfa'ya Git"
         onPress={() => navigation.navigate('Ana Sayfa')}
       />
-      {/* Diğer menü öğeleri ve yönlendirmeleri */}
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center', // Öğeleri dikey olarak ortalar
+    padding: 16, // Yastıklama
+    backgroundColor: '#000000',
+    
+  },
+
+  buttonContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+
+  button: {
+    width: 100, // Genişlik
+    height: 100, // Yükseklik
+    justifyContent: 'center',
+    alignItems: 'center', // Öğeleri dikey olarak ortalar
+    margin: 10,
+    backgroundColor: '#BB994A',
+    
+    borderRadius: 10,
+    marginBottom: 10,  
+  },
+
+
+  title: {
+    fontSize: 20,
+    marginBottom: 20,
+    color: '#BB994A',
+  },
+
+
+  buttonText: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+});
 export default BacakScreen;
